ed4341b1c84b291c25ae70f80f18dfc90d7d060c
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-07-27 18:42:05 +0300

    Dishes initial commit

---
+++ Plates.js
@@ -0,0 +1,28 @@
+class Plates {
+
+}
+
+intro() {
+
+}
+
+buyPlates(count) {
+
+}
+
+usePlates(count) {
+
+}
+
+cleanPlatesCount() {
+
+}
+
+cleanPlatesCount() {
+
+}
+
+cleanUp(count) {
+
+}
+module.exports = Plates;
